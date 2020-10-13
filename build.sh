19765d4e261ad49d284de2d19bf3b82c9984535f
wangpengyuan01 <wangpengyuan01@appledeMacBook-Pro.local>
2020-10-13 20:47:47 +0800

    modify github ci

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ mkdir -p $BUILD_DIR/$BUILD_TYPE\
     && cd $BUILD_DIR/$BUILD_TYPE\
     && cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE\
             $SOURCE_DIR\
-    && make
+    && make\
     && make test
 
